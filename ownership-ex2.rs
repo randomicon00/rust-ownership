3ecefd9ac3d325e351dd0d78f00c4ccde586879a
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2021-06-10 00:02:59 +0100

    Fixing comments

--- ownership-ex2.rs
+++ ownership-ex2.rs
@@ -1,8 +1,7 @@
-
 fn main() {
    let mut mutable_vector = vec![1, 2, 3, 4, 5, 6];
    let reference_to_mutable_vector = &mut mutable_vector //&mut `mutable_vector` borrow starts here
-            // code..
-            // more code..
-}           //&mut `mutable_vector` borrow goes out of scope here
-
+   // code..
+   // more code..
+   
+} //  <---- &mut `mutable_vector` borrow goes out of scope here

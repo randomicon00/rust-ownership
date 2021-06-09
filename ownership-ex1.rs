c2b2b3013155a9678bcc74e446cb61641ae19e2d
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2021-06-09 07:29:14 +0100

    Fixing a typo about mut keyword

--- ownership-ex1.rs
+++ ownership-ex1.rs
@@ -1,6 +1,6 @@
 //Declare a String that allocates `Hello` in the heap. 
 //Its size is then unknown at compile time.
-let word = String::from("Hello");
+let mut word = String::from("Hello");
 
 //Push str function appends a literal ("hardcoded strings") to our word String.
 word.push_str(" there!");

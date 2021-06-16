44c906d18ee143ae32b8c425528c3f90e575f699
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2021-06-17 00:47:49 +0100

    Getting rid of unnecessary spaces

--- ownership-ex1.rs
+++ ownership-ex1.rs
@@ -1,9 +1,7 @@
 //Declare a String that allocates `Hello` in the heap. 
 //Its size is then unknown at compile time.
 let mut word = String::from("Hello");
-
 //Push str function appends a literal ("hardcoded strings") to our word String.
 word.push_str(" there!");
-
 //The print function will output `Hello there!`
 println!("{}", word);

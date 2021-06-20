613da01c7a0759f331ab7485d81c3f13e3c30253
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2021-06-21 00:03:38 +0100

    Multiline doc comment

--- ownership-ex2.rs
+++ ownership-ex2.rs
@@ -1,4 +1,5 @@
-///Rust has no garbage collector. A variable is cleaned up once it goes out of scope. 
+/// Rust has no garbage collector. 
+/// A variable is cleaned up once it goes out of scope. 
 fn main() {
    let mut mutable_vector = vec![1, 2, 3, 4, 5, 6];
    let reference_to_mutable_vector = &mut mutable_vector //&mut `mutable_vector` borrow starts here

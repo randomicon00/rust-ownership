23554e1257c99bcef1074bee3ff6f618304fddbc
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2021-07-17 22:15:27 +0100

    main function is required to run it

--- ownership-ex1.rs
+++ ownership-ex1.rs
@@ -1,8 +1,10 @@
 ///Rust ownership example
 ///Declare a String that allocates `Hello` in the heap. 
-//Its size is then unknown at compile time.
-let mut word = String::from("Hello");
-//Push str function appends a literal ("hardcoded strings") to our word String.
-word.push_str(" there!");
-//The print function will output `Hello there!`
-println!("{}", word);
+///Its size is then unknown at compile time.
+fn main() {
+  let mut word = String::from("Hello");
+  //Push str function appends a literal ("hardcoded strings") to our word String.
+  word.push_str(" there!");
+  //The print function will output `Hello there!`
+  println!("{}", word);
+}

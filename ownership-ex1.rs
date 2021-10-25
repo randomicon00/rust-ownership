148c27d7121646b5d144f3bdd4fca790ebe4bc06
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2021-10-25 21:26:16 +0100

    Added spacing

--- ownership-ex1.rs
+++ ownership-ex1.rs
@@ -1,10 +1,10 @@
-///Rust ownership example
-///Declare a String that allocates `Hello` in the heap. 
-///Its size is then unknown at compile time.
+/// Rust ownership example
+/// Declare a String that allocates `Hello` in the heap. 
+/// Its size is then unknown at compile time.
 fn main() {
   let mut word = String::from("Hello");
-  //Push str function appends a literal ("hardcoded strings") to our word String.
+  // Push str function appends a literal ("hardcoded strings") to our word String.
   word.push_str(" there!");
-  //The print function will output `Hello there!`
+  // The print function will output `Hello there!`
   println!("{}", word);
 }

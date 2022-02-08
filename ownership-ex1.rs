bb8ddf0ddd0fa288cde624fec1c1eda61da8f4e6
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2022-02-08 19:23:49 +0100

    Added comment to mutable declaration

--- ownership-ex1.rs
+++ ownership-ex1.rs
@@ -2,6 +2,7 @@
 /// Declare a String that allocates `Hello` in the heap. 
 /// Its size is then unknown at compile time.
 fn main() {
+  //Get a mutable reference of String which data is located in the heap.
   let mut word = String::from("Hello");
   // Push str function appends a literal ("hardcoded strings") to our word String.
   word.push_str(" there!");

8955592bb363c83e6d76ab464132eff02a54fcc9
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2022-02-12 22:22:32 +0100

    Cleaning up comments

--- ownership-ex2.rs
+++ ownership-ex2.rs
@@ -3,7 +3,7 @@
 /// A variable is cleaned up once it goes out of scope. 
 fn main() {
    let mut mutable_vector = vec![1, 2, 3, 4, 5, 6];
+   
    let reference_to_mutable_vector = &mut mutable_vector //&mut `mutable_vector` borrow starts here
-   // more code..
    
 } //  <---- &mut `mutable_vector` borrow goes out of scope here

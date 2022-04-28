768843d13ffbf6f8b13103b6c3090aeb17ed7a57
randomicon00 <20146907+randomicon00@users.noreply.github.com>
2022-04-28 23:14:51 +0000

    fix: unifying comment style and formatting

--- ownership-ex3.rs
+++ ownership-ex3.rs
@@ -1,21 +1,21 @@
-//leaking resources.
-//unsound behavior in safe rust code.
+// Leaking resources
+// Unsound behavior in safe rust code
 fn main() {
   let mut data = vec![Box::new(0); 4];
   
   {
-    //drain method takes a mutable reference of the vector rendering it inaccessible.
+    // Drain method takes a mutable reference of the vector rendering it inaccessible
     let mut data_drainer = data.drain(..);
     
-    //take two elements and drop them
+    // Take two elements and drop them
     let _ = data_drainer.next();
     let _ = data_drainer.next();
     
-    //now, we just 'forget' about data_drainer without calling its destructor.
-    //It still have two elements at this point.
+    // Now, we just 'forget' about data_drainer without calling its destructor
+    // it still have two elements at this point
     std::mem::forget(data_drainer);
   }
-  //ERROR! basically, at this point, the first and second values of data were previously dropped. 
-  //The data we are trying to read is now free'd memory!
-  //println!("{}", data[0]);  //Uncomment this line.
+  // ERROR! basically, at this point, the first and second values of data were previously dropped
+  // The data we are trying to read is now free'd memory!
+  // println!("{}", data[0]);  // Uncomment this line
 }
